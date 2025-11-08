bd4404418d9a9b3db4b8de2eac58c7d012db352e
p24user23-dot <p24user23@bcpep.org.ua>
2025-11-08 23:12:43 +0200

    fix(api): Import 'authtoken_views' in root urls.py

--- backend/gym_project/urls.py
+++ backend/gym_project/urls.py
@@ -1,6 +1,7 @@
 # gym_project/urls.py
 from django.contrib import admin
-from django.urls import path, include  # Переконайся, що 'include' тут є
+from django.urls import path, include
+from rest_framework.authtoken import views as authtoken_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
